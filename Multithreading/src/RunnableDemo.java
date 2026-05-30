917e536bd7b604988a71a354f52e9d719e5ddbfa
Pratham Mittal <prathamagarwal177@gmail.com>
2026-05-30 07:59:20 +0530

    thread creation using runnable interface

---
+++ Multithreading/src/RunnableDemo.java
@@ -0,0 +1,9 @@
+public class RunnableDemo implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+//            System.out.println("World");
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+}
