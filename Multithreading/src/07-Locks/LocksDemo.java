a7ff1d09b46dc6e1a30a39704a88a53f540eef55
Pratham Mittal <prathamagarwal177@gmail.com>
2026-07-11 02:40:32 +0530

    learned locks - kinds of locks reenterant, fair and unfair locks

---
+++ Multithreading/src/07-Locks/LocksDemo.java
@@ -0,0 +1,42 @@
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Worker extends Thread {
+    private final Lock lock;
+    Worker(String name, Lock lock) {
+        super(name);
+        this.lock = lock;
+    }
+    @Override
+    public void run() {
+        try {
+            if(lock.tryLock(3100, TimeUnit.MILLISECONDS)) {
+                try {
+                    System.out.println(getName() + " acquired lock");
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println(getName() + " could not acquire lock");
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+public class LocksDemo {
+    public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+        Worker t1 = new Worker("Thread-1", lock);
+        Worker t2 = new Worker("Thread-2", lock);
+
+        t1.start();
+        t2.start();
+    }
+}
